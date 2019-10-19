bee85b69b656d6153e6ce4b982a15a1b942a6138
nima habibkhoda <nima.2004hkh@gmail.com>
2019-10-19 22:50:43 +0330

    add search by title or category

--- src/components/NotesList/index.js
+++ src/components/NotesList/index.js
@@ -1,34 +1,92 @@
-import React from "react";
+import React, {useState} from "react";
 import {makeStyles} from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Note from "./Note"
 import {useStateValue} from '../../statemanagement'
+import TextField from '@material-ui/core/TextField';
+import SearchIcon from '@material-ui/icons/Search';
+import InputAdornment from '@material-ui/core/InputAdornment';
 
 const useStyles = makeStyles(theme => ({
   root: {
     flexGrow: 1
   },
   paper: {
-    padding: theme.spacing(2)*2,
+    padding: theme.spacing(2) * 2,
     color: theme.palette.text.secondary,
-    marginBottom:10
+    marginBottom: 10
   },
-  margin:{
-    marginTop:16
+  margin: {
+    marginTop: 8
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1)
   }
 }));
 
 function NotesList() {
   const classes = useStyles();
-  const [{notes},]=useStateValue();
+  const [
+    {
+      notes
+    }
+  ] = useStateValue();
+  const [mainData, setMainData] = useState([]);
+  const [state, setState] = useState()
+  const [stateCategory, setStateCategory] = useState()
+
+  function searchFor(keyword, key, array) {
+    const toSearch = keyword.toLowerCase()
+    return array.filter(data => {
+      return data[key].toLowerCase().includes(toSearch)
+    });
+  }
+
+  function search(event) {
+    const value = event.target.value
+    setState(value)
+    setStateCategory("")
+    const searched = searchFor(value, 'title', notes);
+    if (searched.length > 0) {
+      setMainData(searched)
+    }
+    if (value.length === 0) {
+      setMainData(notes)
+    }
+  }
+
+  function searchCategory(event) {
+    const value = event.target.value
+    setState("")
+    setStateCategory(value)
+    const searched = searchFor(value, 'category', notes);
+    if (searched.length > 0) {
+      setMainData(searched)
+    }
+    if (value.length === 0) {
+      setMainData(notes)
+    }
+  }
+
+  React.useEffect(() => {
+    setMainData(notes);
+  }, [notes])
 
   return (<React.Fragment>
-    <Typography variant="h5" align="center" color="primary" gutterBottom noWrap>Notes </Typography>
+    <TextField value={state} id="outlined-textarea" label="search" placeholder="search title of note" className={classes.textField} margin="normal" variant="outlined" fullWidth="fullWidth" onChange={search} InputProps={{
+        endAdornment: <InputAdornment position="end"><SearchIcon/></InputAdornment>
+      }}/>
+
+    <TextField value={stateCategory} id="outlined-textarea" label="search category" placeholder="search for category" className={[classes.textField,classes.margin].join(" ")} margin="normal" variant="outlined" fullWidth="fullWidth" onChange={searchCategory} InputProps={{
+        endAdornment: <InputAdornment position="end"><SearchIcon/></InputAdornment>
+      }}/>
+
+    <Typography variant="h5" align="center" color="primary" gutterBottom="gutterBottom" noWrap="noWrap">Notes
+    </Typography>
     <div className={classes.margin}>
-      {notes.length > 0 && notes.map((item,index)=>(
-        <Note id={index} item={item} key={item} />
-      ))}
+      {mainData.length > 0 && mainData.map((item, index) => (<Note id={index} item={item} key={item}/>))}
     </div>
   </React.Fragment>)
 }
