51ca2312f16df0a3d5d11dc97df4aeb784aa942f
nima habibkhoda <nima.2004hkh@gmail.com>
2019-10-21 21:12:51 +0330

    add id to rows

--- src/components/CreateNote/index.js
+++ src/components/CreateNote/index.js
@@ -8,78 +8,78 @@ import Select from '@material-ui/core/Select';
 import LocalStorage from "../../Utils/localStorage"
 import {useStateValue} from '../../statemanagement'
 import {useStyles} from './styles'
+import CustomSnackbar from "./Snackbar"
 
 function CreateNote() {
   const classes = useStyles();
-  const [state, setState] = React.useState({category: '', message: '', title:''});
+  const [state, setState] = React.useState({id: 0, category: '', message: '', title: ''});
   const inputLabel = React.useRef(null);
   const [labelWidth, setLabelWidth] = React.useState(0);
-  const [,dispatch]=useStateValue()
+  const [openSnackbar, setOpenSnackbar] = React.useState(false);
+  const [, dispatch] = useStateValue()
 
   React.useEffect(() => {
     setLabelWidth(inputLabel.current.offsetWidth);
   }, []);
 
-  function handleChange(name,event) {
+  function handleChange(name, event) {
     setState({
       ...state,
-      [name]: event.target.value
+      [name]: event.target.value,
+      id: new Date().getTime()
     });
   };
 
   /**
   * Add notes inside of localStorage and context api
   **/
-  function addToNotes(){
-    const allNodes=LocalStorage.getNotes();
-    let allNodesObject=allNodes!==null ?JSON.parse(allNodes): [];
+  function addToNotes() {
+    const allNodes = LocalStorage.getNotes();
+    let allNodesObject = allNodes !== null
+      ? JSON.parse(allNodes)
+      : [];
 
-    if(allNodesObject.length===0){
-      allNodesObject = [state];
+    const rowExists=LocalStorage.rowExists(state);
+    if(rowExists.length === 0){
+      setOpenSnackbar(false)
+      if (allNodesObject.length === 0) {
+        allNodesObject = [state];
+      } else {
+        allNodesObject.push(state);
+      }
+      LocalStorage.setNotes(JSON.stringify(allNodesObject));
+      dispatch({
+        type:'newNote',
+        notes:allNodesObject
+      })
     }
     else{
-      allNodesObject.push(state);
+      setOpenSnackbar(true)
     }
 
-    LocalStorage.setNotes(JSON.stringify(allNodesObject));
-    dispatch({
-      type:'newNote',
-      notes:allNodesObject
-    })
   }
 
   /**
   * On component Did mount , send data from localStorage into context api
   **/
-  React.useEffect(()=>{
-    const Notes=LocalStorage.getNotes();
-    if(Notes!==null){
-      const NoteList=JSON.parse(Notes);
-      dispatch({
-        type:'newNote',
-        notes:NoteList
-      })
+  React.useEffect(() => {
+    const Notes = LocalStorage.getNotes();
+    if (Notes !== null) {
+      const NoteList = JSON.parse(Notes);
+      dispatch({type: 'newNote', notes: NoteList})
     }
-  },[dispatch])
+  }, [dispatch])
 
   return (<React.Fragment>
     <Typography variant="h5" align="center" color="primary" gutterBottom noWrap>Add a new Note</Typography>
 
-    <TextField
-      id="outlined-textarea"
-      label="Title"
-      placeholder="Write your title"
-      className={classes.textField}
-      margin="normal"
-      variant="outlined"
-      fullWidth
-      onChange={(e)=>handleChange('title',e)}/>
+    <TextField id="outlined-textarea" label="Title" placeholder="Write your title" className={classes.textField} margin="normal" variant="outlined" fullWidth onChange={(e) => handleChange('title', e)}/>
 
     <FormControl variant="outlined" className={classes.formControl}>
       <InputLabel ref={inputLabel} htmlFor="outlined-age-native-simple">
         Category
       </InputLabel>
-      <Select native value={state.category} onChange={(e)=>handleChange('category',e)} labelWidth={labelWidth} inputProps={{
+      <Select native value={state.category} onChange={(e) => handleChange('category', e)} labelWidth={labelWidth} inputProps={{
           name: 'age',
           id: 'outlined-age-native-simple'
         }}>
@@ -90,20 +90,11 @@ function CreateNote() {
       </Select>
     </FormControl>
 
-    <TextField
-      id="outlined-textarea"
-      label="Write your Message"
-      placeholder="Write your note"
-      multiline
-      className={classes.textField}
-      margin="normal"
-      variant="outlined"
-      onChange={(e)=>handleChange('message',e)}
-      rows={10}
-      fullWidth/>
+    <TextField id="outlined-textarea" label="Write your Message" placeholder="Write your note" multiline className={classes.textField} margin="normal" variant="outlined" onChange={(e) => handleChange('message', e)} rows={10} fullWidth/>
     <Button variant="outlined" color="primary" className={classes.button} onClick={addToNotes}>
       Add Note
     </Button>
+    <CustomSnackbar open={openSnackbar} setClose={()=>setOpenSnackbar(false)}/>
   </React.Fragment>)
 }
 
