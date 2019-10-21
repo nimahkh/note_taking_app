e222ef5f6925bd4b3e04f1dc1a5b4feff8004c03
nima habibkhoda <nima.2004hkh@gmail.com>
2019-10-21 23:13:57 +0330

    add documentation

--- src/Utils/localStorage.js
+++ src/Utils/localStorage.js
@@ -1,21 +1,36 @@
 const LocalStorage = {};
 
+/**
+* get all notes
+**/
 LocalStorage.getNotes = () => {
   return localStorage.getItem("notes")
 }
 
+/**
+* set a note
+**/
 LocalStorage.set = (key, value) => {
   localStorage.setItem(key, value)
 }
 
+/**
+* set a group of Notes
+**/
 LocalStorage.setNotes = (value) => {
   localStorage.setItem("notes", value)
 }
 
+/**
+* remove Notes
+**/
 LocalStorage.rmNotes =() => {
   localStorage.removeItem('notes')
 }
 
+/**
+* get a note by index
+**/
 LocalStorage.note =(id) => {
   if(LocalStorage.getNotes()!==null){
     const List=JSON.parse(LocalStorage.getNotes());
@@ -24,6 +39,9 @@ LocalStorage.note =(id) => {
   return []
 }
 
+/**
+* check that note exists by id
+**/
 LocalStorage.rowExists= (object)=>{
   const List=JSON.parse(LocalStorage.getNotes());
   if(List!==null && List.length>0){
@@ -36,6 +54,9 @@ LocalStorage.rowExists= (object)=>{
   }
 }
 
+/**
+* get node by id
+**/
 LocalStorage.findId= (id)=>{
   const List=JSON.parse(LocalStorage.getNotes());
   if(List!==null && List.length>0){
@@ -48,6 +69,9 @@ LocalStorage.findId= (id)=>{
   }
 }
 
+/**
+* update a note by id
+**/
 LocalStorage.updateId= (id,itemObject)=>{
   const List=JSON.parse(LocalStorage.getNotes());
   if(List!==null && List.length>0){
